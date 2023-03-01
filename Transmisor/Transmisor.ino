50fdc6de2438940bc6720a0221712718f97b4930
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2023-02-28 19:58:19 -0600

    Transmisor

--- Transmisor/Transmisor.ino
+++ Transmisor/Transmisor.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
+RF24 radio(9, 10); // CE, CSN
+
+byte addresses[][6] = {"node1", "node2"};
+
+void setup() {
+  radio.begin();
+  radio.setChannel(115);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setRetries(15, 15);
+  radio.openWritingPipe(addresses[1]);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  radio.writeFast("PORTADORA", 9);
+  delay(1000);
 }
